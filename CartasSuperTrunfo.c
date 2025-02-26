f05f72e0a62d2a69dc1d83729b4618cb7dc22f57
huguito1312 <hugodoesmfrancisco@gmail.com>
2025-02-26 22:18:18 +0000

    novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,30 +10,36 @@ int main() {
     printf("**Desafio Supertrunfo**\n");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    char codigo[10];
-    char nome[20];
+    char codigo[3];
+    char nome[25];
     int populacao;
     float area;
-    float pib;
+    float PIB;
     int pontos_tur;
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     printf("Digite o código da cidade:\n");
-    scanf("%s", &codigo)
+    scanf("%s", &codigo);
     printf("Digite o nome da cidade:\n");
-    scanf("%s", &codigo)
-    printf("Digite o código da cidade:\n");
-    scanf("%s", &codigo)
-    printf("Digite o código da cidade:\n");
-    scanf("%s", &codigo)
-    printf("Digite o código da cidade:\n");
-    scanf("%s", &codigo)
-    printf("Digite o código da cidade:\n");
-    scanf("%s", &codigo)
+    scanf("%s", &codigo);
+    printf("Digite a população da cidade:\n");
+    scanf("%d", &populacao);
+    printf("Digite a área da cidade:\n");
+    scanf("%f", &area);
+    printf("Digite o PIB da cidade:\n");
+    scanf("%f", &PIB);
+    printf("Digite a quantidade de pontos turísticos da cidade:\n");
+    scanf("%d", &codigo);
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Código: %s\n", codigo);
+    printf("nome: %s\n", nome);
+    printf("População: %d Habitantes\n", populacao);
+    printf("Área: %.2f Km²\n", area);
+    printf("PIB: %.2f\n", PIB);
+    printf("Pontos Turísticos: %d\n", pontos_tur);
 
     return 0;
 }
